44657f051c36f278c4919faec2a7caa88436a19e
Saket S <saket.narayan@vitra.ai>
2022-05-15 21:19:42 +0530

    Adjust search bar size

--- src/Search.js
+++ src/Search.js
@@ -99,8 +99,8 @@ function Search({ selectCoordinates }) {
             viewBox="0 0 24 24"
             stroke="currentColor"
             strokeWidth={2}
-            height={16}
-            width={16}
+            height={18}
+            width={18}
           >
             <path
               strokeLinecap="round"
